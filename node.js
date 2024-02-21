718fe759a739ad6d5361d8150e16106e1f1479c0
salmanyonis1215 <salmanyonis878@gmail.com>
2024-02-21 15:43:38 +0300

    first commit

---
+++ node.js
@@ -0,0 +1,9 @@
+/*let input1 = document.querySelector("#input1")
+let input2 = document.querySelector("#input2")
+
+input1.addEventListener("mouseover", function (){
+    alert("please Enter Your Email Here")
+})
+input2.addEventListener("mouseover", function (){
+    alert(" please Enter Your password Here")
+})//
